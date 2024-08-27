7642aa5c9cbb47aecc7f7e10cddc2e977c23a9c8
D i l s h a n <binoddilshan@gmail.com>
2024-08-27 23:59:20 +0530

    Add Two Text filed for Height and Weight

--- app/src/main/java/com/creative/app/sl/bmicalculator/MainActivity.kt
+++ app/src/main/java/com/creative/app/sl/bmicalculator/MainActivity.kt
@@ -8,12 +8,15 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.systemBarsPadding
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import com.creative.app.sl.bmicalculator.ui.theme.BMICalculatorTheme
 
@@ -45,6 +48,26 @@ fun BMICalculator(){
     ) {
         Text(
             text = "BMI Calculator",
+        )
+        OutlinedTextField(
+            value = "Text",
+            onValueChange = {},
+            label = {
+                Text(
+                    text = "Height"
+                )
+            },
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
+        )
+        OutlinedTextField(
+            value = "Text",
+            onValueChange = {},
+            label = {
+                Text(
+                    text = "Weight"
+                )
+            },
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
         )
     }
 }
